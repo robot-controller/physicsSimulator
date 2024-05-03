ab17cdbe46b731b4cc93890fd6d2d8f42fd80dac
Huelsenfrucht <michael.rothammer@protonmail.com>
2024-05-03 18:57:16 +0200

    wgpu finally runs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,93 @@
+use std::sync::Arc;
+
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
 
 #[derive(Default)]
-struct App {
-    window: Option<Window>,
+struct WGPUApp<'a> {
+    window: Option<Arc<Window>>,
+    surface: Option<wgpu::Surface<'a>>,
+    device: Option<wgpu::Device>,
+    queue: Option<wgpu::Queue>,
+    config: Option<wgpu::SurfaceConfiguration>,
+    size: Option<winit::dpi::PhysicalSize<u32>>,
 }
 
-impl ApplicationHandler for App {
+impl<'a> ApplicationHandler for WGPUApp<'a> {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
+        let window = Arc::new(
             event_loop
                 .create_window(Window::default_attributes())
                 .unwrap(),
         );
+        let wgpu_instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            flags: wgpu::InstanceFlags::DEBUG,
+            dx12_shader_compiler: wgpu::Dx12Compiler::Fxc,
+            gles_minor_version: wgpu::Gles3MinorVersion::Automatic,
+        });
+
+        let surface = wgpu_instance.create_surface(window.clone()).unwrap();
+
+        let size = window.inner_size();
+        let adapter =
+            pollster::block_on(wgpu_instance.request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            }))
+            .unwrap();
+
+        let (device, queue) = pollster::block_on(adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                label: Some("Good device"),
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
+            },
+            None,
+        ))
+        .unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .filter(|format| format.is_srgb())
+            .next()
+            .unwrap_or(surface_capabilities.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        self.window = Some(window);
+        self.surface = Some(surface);
+        self.device = Some(device);
+        self.queue = Some(queue);
+        self.config = Some(config);
+        self.size = Some(size);
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
-        let _ = id;
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed; stopping");
                 event_loop.exit();
+            }
+            WindowEvent::Resized(new_size) => {
+                println!("The window was resized to {:?}", new_size);
+                // self.bruh.as_ref().unwrap().resize(new_size);
             }
             WindowEvent::RedrawRequested => {
                 // Redraw the application.
@@ -38,32 +103,112 @@ impl ApplicationHandler for App {
                 // You only need to call this if you've determined that you need to redraw in
                 // applications which do not always need to. Applications that redraw continuously
                 // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
+                // self.bruh.as_ref().unwrap().update();
+                // match self.bruh.as_ref().unwrap().render() {
+                //     Ok(_) => {}
+                //     // Reconfigure the surface if lost
+                //     Err(wgpu::SurfaceError::Lost) => self
+                //         .bruh
+                //         .as_ref()
+                //         .unwrap()
+                //         .resize(self.bruh.as_ref().unwrap().size),
+                //     // The system is out of memory, we should probably quit
+                //     Err(wgpu::SurfaceError::OutOfMemory) => event_loop.exit(),
+                //     // All other errors (Outdated, Timeout) should be resolved by the next frame
+                //     Err(e) => eprintln!("{:?}", e),
+                // }
             }
             _ => (),
         }
     }
 }
 
-fn main() {
+impl<'a> WGPUApp<'a> {
+    pub fn window(&self) -> &Window {
+        &self.window.as_ref().unwrap()
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = Some(new_size);
+            self.config.as_mut().unwrap().width = new_size.width;
+            self.config.as_mut().unwrap().height = new_size.height;
+            self.surface.as_ref().unwrap().configure(
+                &self.device.as_ref().unwrap(),
+                &self.config.as_ref().unwrap(),
+            );
+        }
+    }
+
+    fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+        // Maybe later
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.as_ref().unwrap().get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder =
+            self.device
+                .as_ref()
+                .unwrap()
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some("Render Encoder"),
+                });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.queue
+            .as_ref()
+            .unwrap()
+            .submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
+
+async fn run() {
     let event_loop = EventLoop::new().unwrap();
 
     // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
     // dispatched any events. This is ideal for games and similar applications.
     event_loop.set_control_flow(ControlFlow::Poll);
 
-    // ControlFlow::Wait pauses the event loop if no events are available to process.
-    // This is ideal for non-game applications that only update in response to user
-    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-    event_loop.set_control_flow(ControlFlow::Wait);
-
-    let mut app = App::default();
-    let wgpu_instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-        backends: wgpu::Backends::PRIMARY,
-        flags: wgpu::InstanceFlags::DEBUG,
-        dx12_shader_compiler: wgpu::Dx12Compiler::Fxc,
-        gles_minor_version: wgpu::Gles3MinorVersion::Automatic,
-    });
+    let mut app = WGPUApp::default();
 
     let _ = event_loop.run_app(&mut app);
+
+    let _bob: f32 = 1.0;
+}
+
+fn main() {
+    pollster::block_on(run());
 }
